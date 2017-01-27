5a68a2de5636b31eb669bb8f5c361b45628fc6c4
Rinku Sharma <rinku.sharma@merrillcorp.com>
2017-01-27 15:22:25 +0530

    Merge branch 'master' into develop

--- src/main/java/com/test/Rinku1.java
+++ src/main/java/com/test/Rinku1.java
@@ -1,11 +1,24 @@
 package com.test;
 
-
 public class Rinku1 {
 
-    
-    
     public void test() {
-        
+
+    }
+
+    public void test1() {
+
+    }
+
+    public void test2() {
+
+    }
+
+    public void test3() {
+
+    }
+
+    public void test4() {
+
     }
 }
